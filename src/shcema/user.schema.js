6b57f96a099dcc32f43953aa5b9578f88c9a476e
Kulsayas <Kulsaya.see@gmail.com>
2024-12-02 00:48:05 +0700

    add: createUser, user schema , userModel
    createUser logic for db, user schema for create user and user login, userModel

---
+++ src/shcema/user.schema.js
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const createUserSchema = z.object({
+  firstname: z.string().min(1, "Name is required"),
+  lastname: z.string().min(1, "Name is required"),
+  email: z.string().email("Invalid email format"),
+  password: z.string().min(8, "Password must be at least 8 characters long"),
+});
+
+export const userLoginSchema = z.object({
+  email: z.string().email("Invalid email format"),
+  password: z.string().min(8, "Password must be at least 8 characters long"),
+});
